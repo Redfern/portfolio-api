19865eff46c4e30cb2bce16a8bac943bf3927bbd
Neil Redfern <redfern@gmail.com>
2025-04-29 21:39:30 +0100

    added to tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmploymentType } from './types/employmentType';
+
+jest.mock('./app.service', () => ({
+  AppService: jest.fn().mockImplementation(() => ({
+    getHello: jest.fn(() => 'Hello World!'),
+    getWorkHistory: jest.fn(() => [
+      {
+        title: 'mocked title',
+        place: 'mocked',
+        years: '2019 - 2021',
+        description: 'hard to say...',
+        employmentType: EmploymentType.Contract,
+      },
+    ]),
+  })),
+}));
 
 describe('AppController', () => {
   let app: TestingModule;
@@ -15,7 +31,19 @@ describe('AppController', () => {
   describe('getHello', () => {
     it('should return "Hello World!"', () => {
       const appController = app.get(AppController);
+
       expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+
+  describe('getWorkHistory', () => {
+    it('should return an array of work places', () => {
+      const appController = app.get(AppController);
+      const result = appController.getWorkHistory();
+
+      expect(result).toBeInstanceOf(Array);
+      expect(result.length).toBeGreaterThan(0);
+      expect(result[0].title).toBe('mocked title');
     });
   });
 });
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,6 +3,7 @@ import { Test } from '@nestjs/testing';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { WorkPlace } from './../src/types/workPlace';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
@@ -25,5 +26,18 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('/work-history (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/work-history')
+      .expect(200)
+      .expect((res) => {
+        const { body }: { body: WorkPlace[] } = res;
+
+        expect(body).toBeInstanceOf(Array);
+        expect(body.length).toBeGreaterThan(0);
+        expect(body[0].title).toBe('C# Full Stack Senior Developer');
+      });
   });
 });
